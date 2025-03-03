50b9f4176141785c7cc9716a81544235cb71149a
Lucio <lu30ya31@hotmail.com>
2025-03-03 15:48:22 -0800

    Broadcast Initial Values

--- Source/Aura/UI/WidgetController/OverlayWidgetController.h
+++ Source/Aura/UI/WidgetController/OverlayWidgetController.h
@@ -6,12 +6,30 @@
 #include "AuraWidgetController.h"
 #include "OverlayWidgetController.generated.h"
 
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnAttributeChangedSignature,float,NewValue);
 /**
  * 
  */
-UCLASS()
+UCLASS(BlueprintType, Blueprintable)
 class AURA_API UOverlayWidgetController : public UAuraWidgetController
 {
 	GENERATED_BODY()
+
+public:
+
+	virtual void BroadcastInitialValues() override;
+
+	UPROPERTY(BlueprintAssignable,Category="GAS|Attributes")
+	FOnAttributeChangedSignature OnHealthChanged;
+
+	UPROPERTY(BlueprintAssignable,Category="GAS|Attributes")
+	FOnAttributeChangedSignature OnMaxHealthChanged;
+
+	UPROPERTY(BlueprintAssignable,Category="GAS|Attributes")
+	FOnAttributeChangedSignature OnManaChanged;
+
+	UPROPERTY(BlueprintAssignable,Category="GAS|Attributes")
+	FOnAttributeChangedSignature OnMaxManaChanged;
 	
 };
